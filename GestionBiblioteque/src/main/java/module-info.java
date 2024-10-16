0544b49949b18ae2f544869a65ecc0df5d5e9b6a
sandwidiabdoulrafi <abdoulrafisandwidi@gmail.com>
2024-10-16 03:59:15 +0000

    fin de la conception de l'interface

--- GestionBiblioteque/src/main/java/module-info.java
+++ GestionBiblioteque/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.gestionbiblioteque {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
     opens com.mycompany.gestionbiblioteque to javafx.fxml;
     exports com.mycompany.gestionbiblioteque;
